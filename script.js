54fcd19c19d1d3d32891a149705642e17747deef
Carolina <n.eugenia.c@gmail.com>
2020-05-16 12:30:31 +0300

    Solved the pesky 'return to zero' problem

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-function runTheClock() {
 var date = new Date();
 console.log(date);
 let hr = date.getHours();
@@ -10,13 +9,19 @@ let min = date.getMinutes();
 let sec = date.getSeconds();
 console.log("Hour: " + hr + "; Minute: " + min + "; Second: " + sec);
 
-let hrPosition = (hr*360/12)+(min*(360/60)/12);
-let minPosition = (min*360/60)+(sec*(360/60)/60);
-let secPosition = sec*360/60;
+let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
+let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
+let secPosition = sec * 360 / 60;
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+function runTheClock() {
+     
+     hrPosition = hrPosition+(3/360); //Hour arm moves the distance of one hour, so 360 over 12, which is 30 over 3600 seconds
+     minPosition = minPosition+(6/60); //We looking for one sixtieh of the degrees of the second hand: (1/60)*6 => 6/60
+     secPosition = secPosition+6; //Divide 360 degrees by 60 seconds = 6 degrees
+
+     HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+     MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+     SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
 }
 
 var interval = setInterval(runTheClock, 1000);
